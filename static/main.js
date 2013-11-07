f68f404222a5c1d4a88c056cfb2d52205b62fc12
ishboo <jacksonegallagher@gmail.com>
2013-11-07 00:08:34 -0500

    removed vestigal code

--- static/main.js
+++ static/main.js
@@ -19,20 +19,3 @@ function update() {
   });
 }
 
-
-//code from http://www.movable-type.co.uk/scripts/latlong.html
-function calculateDistance(lon1, lat1) {
-  // approximate coordinates of the UC
-  var lon2 = -79.942092;
-  var lat2 = 40.443078;
-  var R = 6371; // km
-  var dLat = (lat2-lat1) * Math.PI / 180;
-  var dLon = (lon2-lon1) * Math.PI / 180;
-  var lat1 = lat1 * Math.PI / 180;
-  var lat2 = lat2 * Math.PI / 180;
-
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  return R * c;
-}
